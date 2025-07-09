b0a4eb0e954d36d3897c6b202ece4aced6c176d5
zzthian <zzthian@hotmail.com>
2025-07-09 10:26:41 +0800

    Make dummy data with removed SERP. Remove Click from state.py

--- agent/state.py
+++ agent/state.py
@@ -51,49 +51,28 @@ class Search(StateBase):
 
     def get_thought(self):
         agent = Agent(prompt=StateBase.read_prompt("thought"), **self.prompt_variables)
-        thought = agent.generate()['thought']
+        thought = agent.generate()["thought"]
         return thought
 
     def exec(self):
         thought = self.get_thought()
 
-        agent = Agent(prompt=StateBase.read_prompt("query"), thought=thought, **self.prompt_variables)
-        query = agent.generate()['query']
+        agent = Agent(
+            prompt=StateBase.read_prompt("query"),
+            thought=thought,
+            **self.prompt_variables,
+        )
+        query = agent.generate()["query"]
 
-        self.task.generate_task.append({
-            'step': self.task.step,
-            'query': query,
-            'thought': thought,
-            'real_query': self.task.real_task[self.task.step]['query'],
-            'real_thought': self.task.real_task[self.task.step]['thought'],
-        })
-
-        return Click(self.task)
-
-
-class Click(StateBase):
-    def __init__(self, task):
-        super().__init__(task)
-        self.model = None
-        self.prompt_variables = {
-            "task_description": task_description[self.task.task_id],
-            "history": self.task.get_history_gc(self.task.step).strip(),
-            "thought": self.task.real_task[self.task.step]["thought"],
-            "query": self.task.real_task[self.task.step]["query"],
-            "serp": self.task.get_serp(self.task.step).strip(),
-        }
-
-    def enter(self):
-        pass
-
-    def exec(self):
-        agent = Agent(prompt=StateBase.read_prompt("click"), **self.prompt_variables)
-        results = agent.generate()
-
-        self.task.generate_task[self.task.step].update({
-            'clicks': results['clicks'],
-            'real_clicks': [rank for rank, result in enumerate(self.task.real_task[self.task.step]['SERP']) if result['click'] == 1],
-        })
+        self.task.generate_task.append(
+            {
+                "step": self.task.step,
+                "query": query,
+                "thought": thought,
+                "real_query": self.task.real_task[self.task.step]["query"],
+                "real_thought": self.task.real_task[self.task.step]["thought"],
+            }
+        )
 
         return Stop(self.task)
 
@@ -105,7 +84,6 @@ class Stop(StateBase):
         self.prompt_variables = {
             "task_description": task_description[self.task.task_id],
             "history": self.task.get_history_sc(self.task.step).strip(),
-            "serp": self.task.get_serp(self.task.step).strip(),
         }
 
     def enter(self):
@@ -115,9 +93,11 @@ class Stop(StateBase):
         agent = Agent(prompt=StateBase.read_prompt("stop"), **self.prompt_variables)
         results = agent.generate()
 
-        self.task.generate_task[self.task.step].update({
-            'stop': 1 if '结束会话' in results['action'] else 0,
-        })
+        self.task.generate_task[self.task.step].update(
+            {
+                "stop": 1 if "结束会话" in results["action"] else 0,
+            }
+        )
 
         if self.task.step + 1 == len(self.task.real_task):
             return Finish(self.task)
